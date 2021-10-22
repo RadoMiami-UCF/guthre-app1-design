5c346cde6bca6a4c0f9592eef99c13a59f7a970a
RadoMiami <kguthre@gmail.com>
2021-10-21 21:25:47 -0400

    First commit with basic GUI

---
+++ src/main/java/baseline/Application.java
@@ -0,0 +1,23 @@
+package baseline;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
+        stage.setTitle("Todo List App");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+}
